f55d12e184eea989735fc47ced9f0bba9f031378
Tmk <tmk@li.cm>
2020-09-11 16:29:00 +0800

    style: header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,63 @@
-import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { css } from '@emotion/core';
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    css={css`
+      background: #fff;
+      margin-bottom: 1.45rem;
+      box-shadow: 0px 10px 50px 0px rgba(0, 9, 27, 0.05);
+    `}
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        textAlign: 'right',
-      }}
+      css={css`
+        margin: 0 auto;
+        max-width: 960px;
+        padding: 1.45rem 1.0875rem;
+      `}
     >
-      <h1 style={{ margin: 0, float: 'left' }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+      <div style={{ float: 'right' }}>
+        <ul
+          css={css`
+            list-style: none;
+            margin: 0;
+          `}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-      <div>
-        <ul style={{ listStyle: 'none' }}>
-          <li style={{ display: 'inline-block' }}>
-            <Link style={{ color: '#fff' }} to="/posts/page/1">
+          <li style={{ display: 'inline-block', margin: 0 }}>
+            <Link
+              css={css`
+                color: rgba(0, 0, 0, 0.85);
+              `}
+              to="/posts/page/1"
+            >
               Posts
             </Link>
           </li>
-          <li style={{ display: 'inline-block', marginLeft: 10 }}>
-            <Link style={{ color: '#fff' }} to="/about">
+          <li style={{ display: 'inline-block', margin: '0 0 0 10px' }}>
+            <Link
+              css={css`
+                color: rgba(0, 0, 0, 0.85);
+              `}
+              to="/about"
+            >
               About
             </Link>
           </li>
         </ul>
       </div>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          css={css`
+            color: rgba(0, 0, 0, 0.85);
+            text-decoration: none;
+          `}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
     </div>
   </header>
 );
